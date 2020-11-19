a5251d925f28c48d5cbc3802d7d137db49f03440
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-19 16:43:45 +0530

    Add project files.

---
+++ CabInvoiceGenrator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CabInvoiceGenrator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Cab Invoice Generator");
+        }
+    }
+}
